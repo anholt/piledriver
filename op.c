22a51763c308932894c110cab5514e4d0864a4ba
Michael G. Schwern <schwern@pobox.com>
2014-01-01 18:14:07 -0800

    Turn CHECKOP into an inline function.
    CHECKOP() will take ops of all types, which was presuambly why it was a macro.
    Instead of making the whole function a macro, just make the type casting part a macro.

--- op.c
+++ op.c
@@ -502,16 +502,17 @@ Perl_op_refcnt_dec(pTHX_ OP *o)
     return result;
 }
 #endif
-/*
- * In the following definition, the ", (OP*)0" is just to make the compiler
- * think the expression is of the right type: croak actually does a Siglongjmp.
- */
-#define CHECKOP(type,o) \
-    ((PL_op_mask && PL_op_mask[type])				\
-     ? ( op_free((OP*)o),					\
-         Perl_croak(aTHX_ "'%s' trapped by operation mask", PL_op_desc[type]),	\
-         (OP*)0 )						\
-     : PL_check[type](aTHX_ (OP*)o))
+
+#define CHECKOP(type, o) checkop_real(type, (OP *)o)
+inline OP *checkop_real(I32 type, OP *o) {
+    /* The ", (OP*)0" is just to make the compiler think the expression is of
+       the right type: croak actually does a Siglongjmp. */
+    return((PL_op_mask && PL_op_mask[type])
+     ? ( op_free((OP*)o),
+         Perl_croak(aTHX_ "'%s' trapped by operation mask", PL_op_desc[type]),
+         (OP*)0 )
+     : PL_check[type](aTHX_ (OP*)o));
+}
 
 #define RETURN_UNLIMITED_NUMBER (PERL_INT_MAX / 2)
 
